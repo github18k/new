db96400095164b916c0c399a6d33a8f7476b690c
lwh <632301299@qq.com>
2018-12-18 20:45:01 +0800

    003

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("12332545356");
+        System.out.println("前端人员修改的内容");
     }
 }
